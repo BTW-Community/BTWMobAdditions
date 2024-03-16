5ff03acb2f53a0e6723d0f198db05186127b51ce
ITLSupress <itlesports@outlook.com>
2024-03-15 21:23:30 -0700

    Small Bug Fix Related All Mobs Not Registering

--- src/main/java/com/itlesports/mobadditions/entity/ModEntities.java
+++ src/main/java/com/itlesports/mobadditions/entity/ModEntities.java
@@ -6,13 +6,13 @@ import net.minecraft.src.EntityList;
 
 public class ModEntities {
     public static void createModEntityMappings() {
-        EntityList.addMapping(AshenWolfEntity.class,"ashenwolfEntity", 0,0,0);
-        EntityList.addMapping(BlackWolfEntity.class,"blackwolfEntity", 0,0,0);
-        EntityList.addMapping(ChestnutWolfEntity.class,"chestnutwolfEntity", 0,0,0);
-        EntityList.addMapping(RustyWolfEntity.class,"rustywolfEntity", 0,0,0);
-        EntityList.addMapping(SnowyWolfEntity.class,"snowywolfEntity", 0,0,0);
-        EntityList.addMapping(SpottedWolfEntity.class,"spottedwolfEntity", 0,0,0);
-        EntityList.addMapping(StripedWolfEntity.class,"stripedwolfEntity", 0,0,0);
-        EntityList.addMapping(WoodsWolfEntity.class,"woodswolfEntity", 0,0,0);
+        EntityList.addMapping(AshenWolfEntity.class,"ashenwolfEntity", 701,0,0);
+        EntityList.addMapping(BlackWolfEntity.class,"blackwolfEntity", 702,0,0);
+        EntityList.addMapping(ChestnutWolfEntity.class,"chestnutwolfEntity", 703,0,0);
+        EntityList.addMapping(RustyWolfEntity.class,"rustywolfEntity", 700,0,0);
+        EntityList.addMapping(SnowyWolfEntity.class,"snowywolfEntity", 704,0,0);
+        EntityList.addMapping(SpottedWolfEntity.class,"spottedwolfEntity", 705,0,0);
+        EntityList.addMapping(StripedWolfEntity.class,"stripedwolfEntity", 706,0,0);
+        EntityList.addMapping(WoodsWolfEntity.class,"woodswolfEntity", 707,0,0);
     }
 }
