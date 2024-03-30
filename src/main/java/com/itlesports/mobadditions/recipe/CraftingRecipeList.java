f4a59f7b9cc7bb2b51149b0756b4941187191e40
ITLSupress <itlesports@outlook.com>
2024-03-29 18:33:49 -0700

    Fixed sinewExtractingFox

--- src/main/java/com/itlesports/mobadditions/recipe/CraftingRecipeList.java
+++ src/main/java/com/itlesports/mobadditions/recipe/CraftingRecipeList.java
@@ -2,6 +2,7 @@ package com.itlesports.mobadditions.recipe;
 
 import btw.crafting.recipe.RecipeManager;
 import btw.item.BTWItems;
+import btw.item.items.ProgressiveCraftingItem;
 import com.itlesports.mobadditions.item.ModItems;
 import net.minecraft.src.Item;
 import net.minecraft.src.ItemStack;
@@ -20,13 +21,15 @@ public class CraftingRecipeList {
                 new ItemStack(ModItems.lavaPowder),
                 new ItemStack(BTWItems.glue)
         });
-        RecipeManager.addShapelessRecipe(new ItemStack(ModItems.sinewExtractingFox, 1), new Object[]{
+        RecipeManager.addShapelessRecipe(new ItemStack(ModItems.sinewExtractingFox, 1, ProgressiveCraftingItem.DEFAULT_MAX_DAMAGE), new Object[]{
                 new ItemStack(ModItems.rawFoxChop),
-                new ItemStack(ModItems.rawFoxChop)
+                new ItemStack(ModItems.rawFoxChop),
+                new ItemStack(BTWItems.sharpStone)
         });
-        RecipeManager.addShapelessRecipe(new ItemStack(ModItems.sinewExtractingFox, 1), new Object[]{
+        RecipeManager.addShapelessRecipe(new ItemStack(ModItems.sinewExtractingFox, 1, ProgressiveCraftingItem.DEFAULT_MAX_DAMAGE), new Object[]{
                 new ItemStack(ModItems.cookedFoxChop),
-                new ItemStack(ModItems.cookedFoxChop)
+                new ItemStack(ModItems.cookedFoxChop),
+                new ItemStack(BTWItems.sharpStone)
         });
     }
     public static void addBlockRecipes() {
