9307bb68a803c48fd2f1837df26d9ea42b7ac29d
ITLSupress <itlesports@outlook.com>
2024-03-29 18:20:29 -0700

    Crafting Recipes Fix

--- src/main/java/com/itlesports/mobadditions/recipe/CraftingRecipeList.java
+++ src/main/java/com/itlesports/mobadditions/recipe/CraftingRecipeList.java
@@ -5,31 +5,25 @@ import btw.item.BTWItems;
 import com.itlesports.mobadditions.item.ModItems;
 import net.minecraft.src.ItemStack;
 
-import java.nio.charset.StandardCharsets;
-
 public class CraftingRecipeList {
     public static void addRecipes() {
         addItemRecipes();
         addBlockRecipes();
     }
     public static void addItemRecipes() {
-        RecipeManager.addRecipe(new ItemStack(ModItems.glowPaste, 1), new Object[]{
-                "PG",
-                "P", ModItems.glowPowder,
-                "G", BTWItems.glue
+        RecipeManager.addShapelessRecipe(new ItemStack(ModItems.glowPaste, 1), new Object[]{
+                new ItemStack(ModItems.glowPowder),
+                new ItemStack(BTWItems.glue)
         });
-        RecipeManager.addRecipe(new ItemStack(ModItems.lavaPaste, 1), new Object[]{
-                "PG",
-                "P", ModItems.lavaPowder,
-                "G", BTWItems.glue
+        RecipeManager.addShapelessRecipe(new ItemStack(ModItems.lavaPaste, 1), new Object[]{
+                new ItemStack(ModItems.lavaPowder),
+                new ItemStack(BTWItems.glue)
         });
-        RecipeManager.addRecipe(new ItemStack(ModItems.sinewExtractingFox, 1), new Object[]{
-                "##",
-                "#", ModItems.rawFoxChop
+        RecipeManager.addShapelessRecipe(new ItemStack(ModItems.sinewExtractingFox, 1), new Object[]{
+                new ItemStack(ModItems.rawFoxChop)
         });
-        RecipeManager.addRecipe(new ItemStack(ModItems.sinewExtractingFox, 1), new Object[]{
-                "##",
-                "#", ModItems.cookedFoxChop
+        RecipeManager.addShapelessRecipe(new ItemStack(ModItems.sinewExtractingFox, 1), new Object[]{
+                new ItemStack(ModItems.cookedFoxChop)
         });
     }
     public static void addBlockRecipes() {
